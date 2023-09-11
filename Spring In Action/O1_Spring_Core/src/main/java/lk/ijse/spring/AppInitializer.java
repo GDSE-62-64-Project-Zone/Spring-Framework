a95822ac9e8919772fa51c4f1de94c2aa77e2e3f
sanuv9683 <sanuv9683@gmail.com>
2023-09-11 16:22:34 +0530

    Content Updated

---
+++ Spring In Action/O1_Spring_Core/src/main/java/lk/ijse/spring/AppInitializer.java
@@ -0,0 +1,22 @@
+package lk.ijse.spring;
+
+import lk.ijse.spring.config.AppConfig;
+import lk.ijse.spring.pojo.ImanPojo;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
+
+
+        ImanPojo iman1 = ctx.getBean(ImanPojo.class);
+        ImanPojo iman2 = ctx.getBean(ImanPojo.class);
+
+        iman1.imanIsDancing();
+
+        System.out.println(iman1);
+        System.out.println(iman2);
+    }
+}
---
+++ Spring In Action/O1_Spring_Core/src/main/java/lk/ijse/spring/pojo/ImanPojo.java
@@ -0,0 +1,14 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ImanPojo {
+    public ImanPojo(){
+        System.out.println("Iman Pojja");
+    }
+
+    public void imanIsDancing(){
+        System.out.println("La la la la ala al");
+    }
+}
