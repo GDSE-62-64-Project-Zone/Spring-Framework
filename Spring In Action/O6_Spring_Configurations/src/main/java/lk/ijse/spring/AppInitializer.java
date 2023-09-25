57afb85c5539613475e16ae24f381521e2d13464
sanuv9683 <sanuv9683@gmail.com>
2023-09-25 11:22:57 +0530

    Content Updated

--- Spring In Action/O6_Spring_Configurations/src/main/java/lk/ijse/spring/AppInitializer.java
+++ Spring In Action/O6_Spring_Configurations/src/main/java/lk/ijse/spring/AppInitializer.java
@@ -1,22 +1,22 @@
 package lk.ijse.spring;
 
 import lk.ijse.spring.config.AppConfig;
-import lk.ijse.spring.config.ConfigThree;
-import lk.ijse.spring.config.ConfigTwo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext ctx= new AnnotationConfigApplicationContext();
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
         ctx.register(AppConfig.class);
 
         //so if we want to add more configurations than one. so we can add them
         //using several ways
 
         //01. we can register more config classes using register method
-//        ctx.register(ConfigTwo.class);
-//        ctx.register(ConfigThree.class);
+        //ctx.register(ConfigTwo.class);
+        //ctx.register(ConfigThree.class);
 
+        //02. we can use @import annotation to import configuration class from another configuration class
+        //03 Also we can use @ComponentScan annotation to scan all the Configuration classes
         ctx.refresh();
         ctx.registerShutdownHook();
     }
--- Spring In Action/O6_Spring_Configurations/src/main/java/lk/ijse/spring/config/AppConfig.java
+++ Spring In Action/O6_Spring_Configurations/src/main/java/lk/ijse/spring/config/AppConfig.java
@@ -1,14 +1,12 @@
 package lk.ijse.spring.config;
 
 import lk.ijse.spring.pojo.PojoOne;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.spring.pojo")
-@Import({ConfigTwo.class,ConfigThree.class})
+//@Import({ConfigTwo.class,ConfigThree.class})
+//@ImportResource({"classpath:abcd.xml"})
 public class AppConfig {
     public AppConfig(){
         System.out.println("App Config Instantiated");
