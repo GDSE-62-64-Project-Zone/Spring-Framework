caf79dd69b327ca4769054e8c5f4167b41db1ce7
sanuv9683 <sanuv9683@gmail.com>
2023-09-25 13:48:26 +0530

    addded a new module Spring Environement

--- Spring In Action/O7_Value_Injection/src/main/java/lk/ijse/spring/pojo/PojoOne.java
+++ Spring In Action/O7_Value_Injection/src/main/java/lk/ijse/spring/pojo/PojoOne.java
@@ -2,6 +2,7 @@ package lk.ijse.spring.pojo;
 
 
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -26,16 +27,37 @@ public class PojoOne implements InitializingBean {
 //        System.out.println("PojoOne Instantiated "+id);
 //    }
 
-    public PojoOne(@Value("C001,C002,C003") String[] ids){
-        System.out.println(this.address);//null
+//    public PojoOne(@Value("C001,C002,C003") String[] ids){
+//        System.out.println(this.address);//null
+//
+//        System.out.println("PojoOne Instantiated "+ids);
+//        for (String id : ids) {
+//            System.out.println(id);
+//        }
+//    }
 
-        System.out.println("PojoOne Instantiated "+ids);
-        for (String id : ids) {
-            System.out.println(id);
-        }
+
+    @Autowired(required = false)
+    public PojoOne(@Value("Dasun") String name, @Value("Galle") String address) {
+        System.out.println("Constructor Two");
     }
 
-    @Override
+
+    @Autowired(required = false)
+    public PojoOne(@Value("Dasun") String name,@Value("Galle") String address,@Value("1199") String tp) {
+        System.out.println("Constructor One");
+    }
+
+
+
+
+
+
+
+
+
+
+
     public void afterPropertiesSet() throws Exception {
         System.out.println(this.address);//IJSE Galle
     }
