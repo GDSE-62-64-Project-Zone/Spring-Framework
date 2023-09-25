8ecadab2f85df2e9a2a91eb651054273c762e6c6
sanuv9683 <sanuv9683@gmail.com>
2023-09-25 13:23:39 +0530

    Content Updated

--- Spring In Action/O7_Value_Injection/src/main/java/lk/ijse/spring/pojo/PojoOne.java
+++ Spring In Action/O7_Value_Injection/src/main/java/lk/ijse/spring/pojo/PojoOne.java
@@ -1,11 +1,15 @@
 package lk.ijse.spring.pojo;
 
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PojoOne {
+public class PojoOne implements InitializingBean {
+
+    @Value("Galle IJSE")
+    private String address;
 
 //    public PojoOne(){
 //        System.out.println("PojoOne Instantiated : Default");
@@ -23,9 +27,16 @@ public class PojoOne {
 //    }
 
     public PojoOne(@Value("C001,C002,C003") String[] ids){
+        System.out.println(this.address);//null
+
         System.out.println("PojoOne Instantiated "+ids);
         for (String id : ids) {
             System.out.println(id);
         }
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(this.address);//IJSE Galle
     }
 }
