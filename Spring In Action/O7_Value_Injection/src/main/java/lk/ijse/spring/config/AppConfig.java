b4e9f69c215d87ec2048ac07771045d5f30ee819
sanuv9683 <sanuv9683@gmail.com>
2023-09-25 12:06:36 +0530

    added some methods

--- Spring In Action/O7_Value_Injection/src/main/java/lk/ijse/spring/config/AppConfig.java
+++ Spring In Action/O7_Value_Injection/src/main/java/lk/ijse/spring/config/AppConfig.java
@@ -11,8 +11,13 @@ public class AppConfig {
         System.out.println("AppConfig Instantiated");
     }
 
+    //when we have constructor parameters we have to give values for this
+    //So this is a one way which we can give values for the constructor parameters
 //    @Bean
 //    public String test(){
 //        return "Hello";
 //    }
+
+    //and the second way is we can use @Value annotation to inject values
+    //Runtime to the constructor parameter
 }
