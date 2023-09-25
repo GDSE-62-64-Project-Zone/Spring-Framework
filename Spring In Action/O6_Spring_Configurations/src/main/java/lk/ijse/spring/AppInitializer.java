a81002a3bd518c2fb814ea311d2318e72dc3d74a
sanuv9683 <sanuv9683@gmail.com>
2023-09-25 10:58:05 +0530

    Content Updated with configuration options

--- Spring In Action/O6_Spring_Configurations/src/main/java/lk/ijse/spring/AppInitializer.java
+++ Spring In Action/O6_Spring_Configurations/src/main/java/lk/ijse/spring/AppInitializer.java
@@ -1,12 +1,14 @@
 package lk.ijse.spring;
 
 import lk.ijse.spring.config.AppConfig;
+import lk.ijse.spring.config.ConfigTwo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx= new AnnotationConfigApplicationContext();
         ctx.register(AppConfig.class);
+        ctx.register(ConfigTwo.class);
         ctx.refresh();
         ctx.registerShutdownHook();
     }
--- Spring In Action/O6_Spring_Configurations/src/main/java/lk/ijse/spring/config/AppConfig.java
+++ Spring In Action/O6_Spring_Configurations/src/main/java/lk/ijse/spring/config/AppConfig.java
@@ -1,5 +1,7 @@
 package lk.ijse.spring.config;
 
+import lk.ijse.spring.pojo.PojoOne;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,4 +11,11 @@ public class AppConfig {
     public AppConfig(){
         System.out.println("App Config Instantiated");
     }
+
+
+    @Bean
+    public PojoOne pojoOne(){
+        return new PojoOne();
+    }
+
 }
