3bb274514e2a3cd475c3a46e78c8c3ef83ac8e80
sanuv9683 <sanuv9683@gmail.com>
2023-09-25 11:59:20 +0530

    Content Updated

--- Spring In Action/O7_Value_Injection/src/main/java/lk/ijse/spring/pojo/PojoOne.java
+++ Spring In Action/O7_Value_Injection/src/main/java/lk/ijse/spring/pojo/PojoOne.java
@@ -10,7 +10,22 @@ public class PojoOne {
 //    public PojoOne(){
 //        System.out.println("PojoOne Instantiated : Default");
 //    }
-    public PojoOne(@Value("Hello Spring") String id){
-        System.out.println("PojoOne Instantiated "+id);
+//    public PojoOne(@Value("Hello Spring") String id){
+//        System.out.println("PojoOne Instantiated "+id);
+//    }
+
+//    public PojoOne(@Value("100") int id){
+//        System.out.println("PojoOne Instantiated "+id);
+//    }
+
+//    public PojoOne(@Value("false") boolean id){
+//        System.out.println("PojoOne Instantiated "+id);
+//    }
+
+    public PojoOne(@Value("C001,C002,C003") String[] ids){
+        System.out.println("PojoOne Instantiated "+ids);
+        for (String id : ids) {
+            System.out.println(id);
+        }
     }
 }
