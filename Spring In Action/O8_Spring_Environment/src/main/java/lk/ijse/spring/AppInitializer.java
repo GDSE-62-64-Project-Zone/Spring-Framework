c722a093ae93c20350b884c3736aa63e95be2afe
sanuv9683 <sanuv9683@gmail.com>
2023-09-25 14:59:57 +0530

    Content Updated

--- Spring In Action/O8_Spring_Environment/src/main/java/lk/ijse/spring/AppInitializer.java
+++ Spring In Action/O8_Spring_Environment/src/main/java/lk/ijse/spring/AppInitializer.java
@@ -3,11 +3,31 @@ package lk.ijse.spring;
 import lk.ijse.spring.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Map;
+import java.util.Properties;
+
 public class AppInitializer {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
         ctx.register(AppConfig.class);
         ctx.refresh();
         ctx.registerShutdownHook();
+
+
+        //get system environment variables
+//        Map<String, String> env = System.getenv();
+//        for (String key : env.keySet()) {
+//            String value = env.get(key);
+//            System.out.println(key + " - "+value);
+//        }
+//        System.out.println("==========================");
+//
+//        //get java environment variables
+//        Properties properties = System.getProperties();
+//        for (Object key : properties.keySet()) {
+//            Object value = properties.get(key);
+//            System.out.println(key+"-"+value);
+//        }
+
     }
 }
--- Spring In Action/O8_Spring_Environment/src/main/java/lk/ijse/spring/pojo/PojoOne.java
+++ Spring In Action/O8_Spring_Environment/src/main/java/lk/ijse/spring/pojo/PojoOne.java
@@ -1,10 +1,48 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PojoOne {
+public class PojoOne implements InitializingBean {
+
+    //${} - Property Placeholder
+    @Value("${USER}")
+    private String userName;
+
+    @Value("${os.name}")
+    private String osName;
+
+    @Value("${url}")
+    private String url;
+
+    @Autowired
+    Environment env;
+
     public PojoOne(){
         System.out.println("Pojo One Instantiated");
+    }
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(this.userName);
+        System.out.println(this.osName);
+        System.out.println(this.url);
+
+
+        System.out.println("===========");
+        //Will return a null if the key is wrong or missing
+        String user1 = env.getProperty("USSER");
+
+        //will return an exception if the key is missing or wrong
+        String user2 = env.getRequiredProperty("urls");
+
+        System.out.println(user1);
+        System.out.println(user2);
+
     }
 }
