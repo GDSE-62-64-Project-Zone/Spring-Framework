1da86f16fa153e468e61c6d248efbd116efacd31
sanuv9683 <sanuv9683@gmail.com>
2023-09-30 15:59:26 +0530

    Content Updated

--- Spring In Action/O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/E_Query_Sting_Parameters_Controller.java
+++ Spring In Action/O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/E_Query_Sting_Parameters_Controller.java
@@ -8,27 +8,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/query")
 public class E_Query_Sting_Parameters_Controller {
-//When we have ambiguous mapping we can handle it using
-//Query String parameters also
-//params = {"id","name"} @RequestParam("id")
+    //When we have ambiguous mapping we can handle it using
+    //Query String parameters also
+    //params = {"id","name"} @RequestParam("id")
 
 
-//    @RequestParam -> not required
+    //@RequestParam -> not required
 
-//http://localhost:8080/mapping/validate?id=C001&name=Dasun
+    //Query String parameters
+    //http://localhost:8080/mapping/validate?id=C001&name=Dasun
 
     @GetMapping
-    public String getMapping2(){
+    public String getMapping2() {
         return "Get Mapping Invoked 2";
     }
 
     //?id=C001&name=Kasun&address=Galle
-    @GetMapping(params = {"id","name","address"})
-    public String getMapping1(@RequestParam String id,String name,String address){
-        return "Get Mapping Invoked 1 "+id+" "+name+ " "+address;
+    @GetMapping(params = {"id", "name", "address"})
+    public String getMapping1(@RequestParam String id, String name, String address) {
+        return "Get Mapping Invoked 1 " + id + " " + name + " " + address;
     }
-
-
 
 
 }
