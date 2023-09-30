76b17ccfcce4512895efd6045f8b90ea5f53d087
sanuv9683 <sanuv9683@gmail.com>
2023-09-30 12:53:34 +0530

    added a new module for rest controllers

---
+++ Spring In Action/O11_REST_Controllers/src/main/java/lk/ijse/spring/controller/RestController.java
@@ -0,0 +1,9 @@
+package lk.ijse.spring.controller;
+
+public class RestController {
+
+    //No need of view resolvers
+    //Will return anything you return by wrapping it into a HTTP response if compatible
+
+    //Ambiguous Mapping
+}
