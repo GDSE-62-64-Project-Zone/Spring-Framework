d0606cbc6bbfabc183a0a69b5e9eed945f23e37a
sanuv9683 <sanuv9683@gmail.com>
2023-09-30 14:15:27 +0530

    content updated

--- Spring In Action/O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/B_Path_Segments_Controller.java
+++ Spring In Action/O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/B_Path_Segments_Controller.java
@@ -1,13 +1,26 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/segment")
 public class B_Path_Segments_Controller {
 //Ways of Ambiguous Mappings Resolving Methods
 //01 Use path segments to narrow down requests
 //path="/customer/add"
 //localhost:8080/mapping/customer/add
 //localhost:8080/mapping/customer/delete
+
+    @GetMapping(path = "/one")
+    public String getMapping1(){
+        return "Get Mapping Invoked 1";
+    }
+
+    @GetMapping
+    public String getMapping2(){
+        return "Get Mapping Invoked 2";
+    }
 
 }
