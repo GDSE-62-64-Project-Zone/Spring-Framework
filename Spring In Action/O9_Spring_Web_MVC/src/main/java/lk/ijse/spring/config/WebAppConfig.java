08c4d03779329fac8fe5831aacd622e4fa516920
sanuv9683 <sanuv9683@gmail.com>
2023-09-30 09:11:19 +0530

    Content Updated with Spring Web app Initilization

--- Spring In Action/O9_Spring_Web_MVC/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ Spring In Action/O9_Spring_Web_MVC/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -1,5 +1,9 @@
 package lk.ijse.spring.config;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -7,5 +11,34 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.spring.pojo")
-public class WebAppConfig {
+public class WebAppConfig implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
+    public WebAppConfig(){
+        System.out.println("WebAppConfig : Instantiated");
+    }
+
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("WebAppConfig : Bean Factory Awareness");
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("WebAppConfig : Bean Name Awareness");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("WebAppConfig : Disposable Bean");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("WebAppConfig : Initializing Bean");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("WebAppConfig : Application Context Awareness");
+    }
 }
