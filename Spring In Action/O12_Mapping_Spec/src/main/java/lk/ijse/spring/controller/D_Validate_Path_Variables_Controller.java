a82d0d685fb46055a8ad0dd01e69321b1a516914
sanuv9683 <sanuv9683@gmail.com>
2023-09-30 14:46:54 +0530

    Content Updated

--- Spring In Action/O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/D_Validate_Path_Variables_Controller.java
+++ Spring In Action/O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/D_Validate_Path_Variables_Controller.java
@@ -1,5 +1,7 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +11,21 @@ public class D_Validate_Path_Variables_Controller {
 //02.Single Character Validations path={/pa?h}
 //03.Single Wild Card Validator path={/path/*/id}
 //04Multiple Wild Card Validator path={/path/**/id}
+
+
+    //path segments
+    @GetMapping(path = "/i?d")
+    public String getMapping2(@PathVariable("id") String ids){ //alies
+        System.out.println(ids);
+        return "Get Mapping Invoked 1 "+ids;
+    }
+
+    //request narrow downing using path segments
+    @GetMapping(path = "/id??/nam??e")
+    public String getMapping1(@PathVariable String id,@PathVariable String name){
+        System.out.println(id+" "+name);
+        return "Get Mapping Invoked 2 "+id+" "+name;
+    }
+
+
 }
