28dfa88d1ffd3a7342a20843870535c1d023ffa2
sanuv9683 <sanuv9683@gmail.com>
2023-09-30 11:06:00 +0530

    Content Updated

--- Spring In Action/O9_Spring_Web_MVC/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ Spring In Action/O9_Spring_Web_MVC/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,14 +1,17 @@
 package lk.ijse.spring.controller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/customer")
+@RequestMapping("/customer")//handler mapping
 public class CustomerController {
 
+    //handler methods
+    //handler mapping
     @GetMapping
     public String helloSpring(){
         return "Hello Iam Spring: Your Request Received";
