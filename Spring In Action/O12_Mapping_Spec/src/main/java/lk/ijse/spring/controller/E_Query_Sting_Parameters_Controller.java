fd1be1658211f825d43a7c3d1db83b74693bf4a4
sanuv9683 <sanuv9683@gmail.com>
2023-09-30 15:46:10 +0530

    Content Updated

--- Spring In Action/O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/E_Query_Sting_Parameters_Controller.java
+++ Spring In Action/O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/E_Query_Sting_Parameters_Controller.java
@@ -1,10 +1,30 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/query")
 public class E_Query_Sting_Parameters_Controller {
 //When we have ambiguous mapping we can handle it using
 //Query String parameters also
 //params = {"id","name"} @RequestParam("id")
+
+
+//http://localhost:8080/mapping/validate?id=C001&name=Dasun
+
+    @GetMapping()
+    public String getMapping2(){
+        return "Get Mapping Invoked 2";
+    }
+
+    @GetMapping(params = {"id"})
+    public String getMapping1(String id){
+        return "Get Mapping Invoked 1 "+id;
+    }
+
+
+
+
 }
