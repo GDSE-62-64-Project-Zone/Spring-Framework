fbacf8ba0cdc61f8f2927365280f8fd242b1605f
sanuv9683 <sanuv9683@gmail.com>
2023-09-30 16:30:04 +0530

    Content Updated

--- Spring In Action/O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/F_Request_Headers_Controller.java
+++ Spring In Action/O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/F_Request_Headers_Controller.java
@@ -1,8 +1,13 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/headers")
 public class F_Request_Headers_Controller {
     //Also we can narrow down a request using request headers also
     //Every Request Consists with two parts
@@ -13,4 +18,27 @@ public class F_Request_Headers_Controller {
     //consumes = {MediaType}, this will require content-type header from front end
     //produce = Accept
     //headers = {"Content-Type=application/json","Accept=text/html"}
+
+
+    //here we can narrow down a request using request headers
+    //the thing which happen here is using the request header (Content-Type)
+    //you can choose what handler method to execute
+
+    //if the request is sent without content type header then
+    //there will be an error called unsupported media type (415)
+
+
+    //to invoke this you have to send content-type=application/json from the fornt
+    //end
+    @GetMapping(consumes = "application/json")
+    public String getMapping1() {
+        return "Get Mapping Invoked 1";
+    }
+
+    //to invoke this you have to send content-type=text/html from the fornt
+    //end
+    @GetMapping(consumes = "text/html")
+    public String getMapping2() {
+        return "Get Mapping Invoked 2";
+    }
 }
