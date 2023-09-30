66bf8eb21d1e3635ac23cbafe07037c8d98d5410
sanuv9683 <sanuv9683@gmail.com>
2023-09-30 13:14:31 +0530

    Content Updated

---
+++ Spring In Action/O11_REST_Controllers/src/main/java/lk/ijse/spring/controller/RestArchController.java
@@ -0,0 +1,22 @@
+package lk.ijse.spring.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class RestArchController {
+
+    //No need of view resolvers
+    //Will return anything you return by wrapping it into a HTTP response if compatible
+
+    //Ambiguous Mapping
+
+
+    @GetMapping
+    public void test(){
+        System.out.println("Test method invoked");
+    }
+}
--- Spring In Action/O11_REST_Controllers/src/main/java/lk/ijse/spring/controller/RestController.java
+++
@@ -1,9 +0,0 @@
-package lk.ijse.spring.controller;
-
-public class RestController {
-
-    //No need of view resolvers
-    //Will return anything you return by wrapping it into a HTTP response if compatible
-
-    //Ambiguous Mapping
-}
