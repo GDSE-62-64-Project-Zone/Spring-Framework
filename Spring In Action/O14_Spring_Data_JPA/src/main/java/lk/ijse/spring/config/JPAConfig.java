a15daf9a75a0ea3dbbcdab5a26fd26cad68e5826
sanuv9683 <sanuv9683@gmail.com>
2023-10-02 15:20:15 +0530

    Content Updated

--- Spring In Action/O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ Spring In Action/O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -4,9 +4,14 @@ package lk.ijse.spring.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
@@ -32,6 +37,22 @@ public class JPAConfig {
         ds.setDriverClassName("com.mysql.jdbc.Driver");
         ds.setUrl("jdbc:mysql://localhost:3306/d2?createDatabaseIfNotExist=true");
         return ds;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+        HibernateJpaVendorAdapter va= new HibernateJpaVendorAdapter();
+        va.setDatabase(Database.MYSQL);
+        va.setGenerateDdl(true);
+        va.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+        va.setShowSql(true);
+        return va;
+    }
+
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory factory){
+        return new JpaTransactionManager();
     }
 
 }
