13d4ae577badd7a3a0b71d94ab33f3f8950163ce
sanuv9683 <sanuv9683@gmail.com>
2023-10-02 13:47:30 +0530

    Content Updated

--- Spring In Action/O13_Spring_with_JSON/src/main/java/lk/ijse/spring/advisor/AppWideExceptionHandler.java
+++ Spring In Action/O13_Spring_with_JSON/src/main/java/lk/ijse/spring/advisor/AppWideExceptionHandler.java
@@ -4,9 +4,14 @@ import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+//this is also a type of aspect
+//this class will handle all the runtime exceptions of the application
 @RestControllerAdvice
 public class AppWideExceptionHandler {
 
+    //if any runtime error generated inside the container object below
+    //method will handle it and generate a response as we wish
+    //here we have used the Response Util Class to generate a json object
     @ExceptionHandler({RuntimeException.class})
     public ResponseUtil handleAllRuntimeExceptions(RuntimeException e){
         return new ResponseUtil("Error",e.getMessage(),null);
