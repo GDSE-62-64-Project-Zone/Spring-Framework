f5d81f0860aea556e84027e28ea3ae2912b79e97
sanuv9683 <sanuv9683@gmail.com>
2023-10-02 11:21:15 +0530

    Content Updated

---
+++ Spring In Action/O13_Spring_with_JSON/src/main/java/lk/ijse/spring/controller/E_CustomerController.java
@@ -0,0 +1,18 @@
+package lk.ijse.spring.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+public class E_CustomerController {
+
+    //add
+    //delete
+    //update
+    //search
+    //get_all
+
+//    return
+}
