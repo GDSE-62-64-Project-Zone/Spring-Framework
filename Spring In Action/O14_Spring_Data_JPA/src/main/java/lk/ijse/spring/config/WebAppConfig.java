612578440c2a013d50eecd17f81f783c438a04f8
sanuv9683 <sanuv9683@gmail.com>
2023-10-09 11:50:36 +0100

    Content Updated

--- Spring In Action/O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ Spring In Action/O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "lk.ijse.spring.controller")
+@ComponentScan(basePackages = {"lk.ijse.spring.controller","lk.ijse.spring.advisor"})
 //@ComponentScan(basePackageClasses = {CustomerController.class})//will scan CustomerController class package and all of its sub packages inside that package
 public class WebAppConfig {
     public WebAppConfig(){
