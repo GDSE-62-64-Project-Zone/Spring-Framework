eb6c997d0264d42476045154fd61bf6e76cd2bcb
sanuv9683 <sanuv9683@gmail.com>
2023-10-09 12:20:30 +0100

    Content Updated

--- Spring In Action/O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/advisor/AppWideExceptionHandler.java
+++ Spring In Action/O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/advisor/AppWideExceptionHandler.java
@@ -2,12 +2,14 @@ package lk.ijse.spring.advisor;
 
 import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import sun.security.provider.certpath.OCSPResponse;
 
 @RestControllerAdvice
+@CrossOrigin
 public class AppWideExceptionHandler {
 
     @ExceptionHandler({RuntimeException.class})
--- Spring In Action/O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/ItemController.java
+++ Spring In Action/O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/ItemController.java
@@ -3,12 +3,14 @@ package lk.ijse.spring.controller;
 import lk.ijse.spring.dto.ItemDTO;
 import lk.ijse.spring.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/item")
+@CrossOrigin // will support to cors requests
 public class ItemController {
 
     @Autowired
