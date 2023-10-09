c9a9e3368e18d64b0a66b3dd347c449fcf3cebb0
sanuv9683 <sanuv9683@gmail.com>
2023-10-09 10:04:08 +0100

    Content Updated

---
+++ Spring In Action/O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/service/impl/ItemBOImpl.java
@@ -0,0 +1,8 @@
+package lk.ijse.spring.service.impl;
+
+import lk.ijse.spring.service.ItemBO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemBOImpl implements ItemBO {
+}
