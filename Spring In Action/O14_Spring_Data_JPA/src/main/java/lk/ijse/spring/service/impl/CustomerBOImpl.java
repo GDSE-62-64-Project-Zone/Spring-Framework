98c87c7774b26574e3b43d16d3703c1c966aa841
sanuv9683 <sanuv9683@gmail.com>
2023-10-09 10:05:04 +0100

    Content Updated

--- Spring In Action/O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/service/impl/CustomerBOImpl.java
+++ Spring In Action/O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/service/impl/CustomerBOImpl.java
@@ -3,6 +3,9 @@ package lk.ijse.spring.service.impl;
 import lk.ijse.spring.service.CustomerBO;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@Transactional
 public class CustomerBOImpl implements CustomerBO {
 }
--- Spring In Action/O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/service/impl/ItemBOImpl.java
+++ Spring In Action/O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/service/impl/ItemBOImpl.java
@@ -3,6 +3,9 @@ package lk.ijse.spring.service.impl;
 import lk.ijse.spring.service.ItemBO;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@Transactional // manage all the transactions here
 public class ItemBOImpl implements ItemBO {
 }
