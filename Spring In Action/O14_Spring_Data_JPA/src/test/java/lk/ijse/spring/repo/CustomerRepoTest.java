659991f62d4812e67d6cc02f3d06c05ef843e468
sanuv9683 <sanuv9683@gmail.com>
2023-10-10 10:46:57 +0530

    Contetnt Updated

--- Spring In Action/O14_Spring_Data_JPA/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
+++ Spring In Action/O14_Spring_Data_JPA/src/test/java/lk/ijse/spring/repo/CustomerRepoTest.java
@@ -10,13 +10,14 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@WebAppConfiguration
-@ContextConfiguration(classes = {WebRootConfig.class})
-@ExtendWith(SpringExtension.class)
+@WebAppConfiguration // create testing context
+@ContextConfiguration(classes = {WebRootConfig.class}) //load configurations which wanted for test context
+@ExtendWith(SpringExtension.class) //We have to use Jupiter with Spring Test as a vendor
 class CustomerRepoTest {
 
     @Autowired
@@ -28,6 +29,12 @@ class CustomerRepoTest {
         for (Customer customer : all) {
             System.out.println(customer.toString());
         }
+    }
+
+    @Test
+    public void testAddCustomer(){
+        Customer customer = new Customer("C004", "Iman", "Galle", new BigDecimal(100.00));
+        repo.save(customer);
     }
 
 }
