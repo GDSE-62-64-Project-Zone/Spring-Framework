f9e6a511417224870599559be445785ec16bf3c5
sanuv9683 <sanuv9683@gmail.com>
2023-10-11 10:16:43 +0530

    Content Updated

--- Spring In Action/O15_Spring_Boot/src/main/java/lk/ijse/spring/WebAppInitializer.java
+++ Spring In Action/O15_Spring_Boot/src/main/java/lk/ijse/spring/WebAppInitializer.java
@@ -2,21 +2,16 @@ package lk.ijse.spring;
 
 import lk.ijse.spring.config.WebAppConfig;
 import lk.ijse.spring.config.WebRootConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[]{WebRootConfig.class};
+
+@SpringBootApplication
+public class WebAppInitializer{
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppInitializer.class);
     }
 
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class[]{WebAppConfig.class};
-    }
 
-    @Override
-    protected String[] getServletMappings() {
-        return new String[]{"/"};
-    }
 }
